bbab0855d0aafb5f9ea22319a168f1fc83cbf561
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-07-23 09:04:18 +1200

    Modified to disable the touchpad

--- set_mouse_scroll.sh
+++ set_mouse_scroll.sh
@@ -6,3 +6,7 @@ do
   xinput set-int-prop $input_id "Evdev Wheel Emulation Button" 8 2
   xinput set-int-prop $input_id "Evdev Wheel Emulation Axes" 8 6 7 4 5
 done
+
+# Disable touchpad
+TOUCHPAD_ID=$(xinput | grep 'SynPS/2 Synaptics TouchPad' | sed 's%.*id=\([0-9]\+\).*%\1%')
+xinput set-prop $TOUCHPAD_ID 'Device Enabled' 0
